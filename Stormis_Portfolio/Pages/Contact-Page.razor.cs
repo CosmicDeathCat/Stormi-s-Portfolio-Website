3a1ade2adc8655db84db0d8e2092977bd4088c13
Stormi Dragon <Thegreatjinxycat@gmail.com>
2024-05-10 02:08:43 -0500

    Added Skills Page Lots of edits
    Added skills page with responsive CSS and made it look good with the homepage and nav bar too.

---
+++ Stormis_Portfolio/Pages/Contact-Page.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Stormis_Portfolio.Pages;
+
+public partial class Contact_Page : ComponentBase
+{
+    ContactForm contactForm = new();
+}
+public class ContactForm
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+}
---
+++ Stormis_Portfolio/wwwroot/js/app.js
@@ -0,0 +1,8 @@
+﻿window.initSkillsPage = function () {
+    const skills = document.querySelectorAll('.skills-category');
+    skills.forEach((skill, index) => {
+        setTimeout(() => {
+            skill.classList.add('slide-in');
+        }, index * 300);
+    });
+};
