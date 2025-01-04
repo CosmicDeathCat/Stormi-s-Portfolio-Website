f33f80d35078508dbd1b02a7f4c52c58b05e95c6
Nicole Hamilton <thegreatjinxycat@gmail.com>
2025-01-04 13:28:03 -0600

    Revert "Updated Site"
    This reverts commit a6ab6070419094405e75c773ad69fc57aefbf968.

--- Stormis_Portfolio/Pages/Contact-Page.razor.cs
+++ Stormis_Portfolio/Pages/Contact-Page.razor.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Components.Forms;
 using Stormis_Portfolio.Models;
 using Stormis_Portfolio.Services;
-using Stormis_Portfolio.Templates.Email;
-using System.Collections.Concurrent;
 
 namespace Stormis_Portfolio.Pages;
 
@@ -13,38 +11,13 @@ public partial class Contact_Page : ComponentBase
     ContactForm contactForm = new();
     private string toAddress = "StormiDragon090@gmail.com";
     private string fromAddress = "donotreply@b6d1fa2c-effa-4597-b2ed-23ed3f6a56bd.azurecomm.net";
-    private bool isSubmitted = false;
-    private string message = "Thank you for contacting me! I'll be in touch soon!";
-
-    // Dictionary to store the last submission time for each email address
-    private static ConcurrentDictionary<string, DateTime> lastSubmissionTimes = new();
-    // private TimeSpan submissionInterval = TimeSpan.FromHours(1); // Configurable interval
-    private TimeSpan submissionInterval = TimeSpan.FromHours(1); // Configurable interval
 
     private async Task HandleValidSubmit(EditContext arg)
     {
         try
         {
-            string userKey = contactForm.Email; // Using email as the key
-            DateTime now = DateTime.UtcNow;
-
-            if (lastSubmissionTimes.TryGetValue(userKey, out DateTime lastSubmissionTime))
-            {
-                if (now - lastSubmissionTime < submissionInterval)
-                {
-                    message = "You have already submitted a contact form. Please wait at least 1 hour before submitting another.";
-                    return;
-                }
-            }
-
-            message = "Thank you for contacting me! I'll be in touch soon!";
-
-            // Update the last submission time
-            lastSubmissionTimes[userKey] = now;
-
-            // Send the email
-            await emailService.SendEmailAsync(fromAddress, toAddress, contactForm.Subject, ContactFormEmailTemplate.ContactFormEmail(contactForm));
-            isSubmitted = true;
+            string emailBody = $"Name: {contactForm.Name}\nEmail: {contactForm.Email}\nPhone Number: {contactForm.PhoneNumber}\nSubject: {contactForm.Subject}\nContact Reason: {contactForm.ContactReason}\nMessage: {contactForm.Message}";
+            await emailService.SendEmailAsync(fromAddress, toAddress, contactForm.Subject, emailBody);
         }
         catch (Exception e)
         {
